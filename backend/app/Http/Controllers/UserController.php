c565bb36f3877728aada9525302d12078cbb7826
AfamsVal <progfams@gmail.com>
2023-12-28 14:46:36 +0100

    migration with book controller

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -5,23 +5,61 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
-    //
+
+
     function register(Request $req)
     {
+        $validator = validator($req->all(), [
+            'firstName' => 'required|string|max:255',
+            'lastName' => 'required|string|max:255',
+            'email' => [
+                'required',
+                'email',
+                'max:255',
+                Rule::unique('users', 'email'),
+            ],
+            'password' => [
+                'required',
+                'string',
+                'min:8',
+                'regex:/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]+$/',
+            ],
+        ], [
+            'password.regex' => 'The password should be in the format: at least one alphabet character, one digit, and one special character.',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
+
         $user = new User;
         $user->firstName = $req->input('firstName');
         $user->lastName = $req->input('lastName');
         $user->email = $req->input('email');
         $user->password = Hash::make($req->input('password'));
         $user->save();
+
         return response()->json($user);
     }
 
+
+
+
     function login(Request $req)
     {
+        $validator = validator($req->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
+
         $user = User::where('email', $req->email)->first();
         if (!$user || !Hash::check($req->password, $user->password)) {
             return ['error' => 'Incorrect email or password!'];
